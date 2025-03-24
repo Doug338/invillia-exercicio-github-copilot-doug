07060ca9e011057138baf769e2152fc67e89b8fd
Doug338 <douglassilva338@gmail.com>
2025-03-24 16:14:28 +0000

    Adiciona validação para limite de participantes na inscrição em atividades

--- src/app.py
+++ src/app.py
@@ -103,7 +103,9 @@ def signup_for_activity(activity_name: str, email: str):
     # Check if student is already signed up
     if email in activity["participants"]:
         raise HTTPException(status_code=400, detail="Student already signed up for this activity")
-
+    #Validate student is not already signed up
+    if len(activity["participants"]) >= activity["max_participants"]:
+        raise HTTPException(status_code=400, detail="Activity is full")
     # Add student
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
